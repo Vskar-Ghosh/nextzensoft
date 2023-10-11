34d3b9f63afce4e99be7f92d65a0f6e2c59e73a4
vaskar-ghosh <vaskar.ghosh.99.23@gmail.com>
2023-10-11 20:09:39 +0300

    after responsive from chatgpt

--- src/components/HomePageCompnents.js/WhatWeDo.js
+++ src/components/HomePageCompnents.js/WhatWeDo.js
@@ -18,7 +18,7 @@ const WhatWeDo = () => {
 
   return (
     <div
-      className=" min-w-full max-h-[70vh] flex items-center justify-center text-white mt-20"
+      className=" min-w-full max-h-[70vh] flex items-center justify-between text-white mt-20"
       style={backgroundImageStyle}
     >
       <div className="w-full h-full md:w-1/2 md:h-1/2 lg:w-1/2 lg:h-1/2 xl:w-1/3 xl:h-1/3 p-4 mx-5">
@@ -29,11 +29,11 @@ const WhatWeDo = () => {
         />
       </div>
       {/* End big image div */}
-      <div className="w-full max-h-[50%] p-4 mx-5 relative">
-        <h1 className="font-adventPro text-2xl md:text-3xl lg:text-2xl xl:text-sm">
+      <div className="w-full md:w-1/2 lg:w-1/2 xl:w-2/5 p-4 mx-5 relative">
+        <h1 className="font-adventPro text-2xl md:text-3xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
           What We Do
         </h1>
-        <p className="text-[#FFFFFF] font-adventPro text-base md:text-lg xl:text-sm mt-2 md:mt-4">
+        <p className="text-white font-adventPro text-base md:text-lg xl:text-xl mt-2 md:mt-4">
           We build software development teams that grow with our clients, from
           the early stages to whatever they aspire to be. Our expertise includes
           front-end, backend, database, and server solutions for mobile, web,
